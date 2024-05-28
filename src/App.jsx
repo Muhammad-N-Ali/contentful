1e97bc9f052a6012fb270de06e171841e64ce527
Muhammad-N-Ali <m.naeemali572@gmail.com>
2024-05-28 09:52:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+// import localData from './Data/Data'
+// const contentful = require('contentful')
+import {createClient} from 'contentful'
+
+function App() {
+  const [data, setData] = useState([])
+
+  // make  veriables for space and accessToken and store their values
+
+  const space = '795o7smso5da'
+  const accessToken = 'HRygj7MR0JNIAH33NYb8JQCbUhgpbWP8D0zGMWF5gGY'
+
+ 
+//to create the connection with backend
+const client = createClient({
+  space,
+  environment: 'master', // defaults to 'master' if not set
+  accessToken
+})
+
+
+
+// const getAll = ()=> { 
+//   client.getEntries()
+//   .then((response) => setData(response.items))
+//   .catch(console.error)
+//   } 
+// now we are using try and catch for above code 25 to 29
+// the try catch mathod is useful for pagination 
+const getUser = async (type="user", limit=10, skip=0) => {
+  try{
+    const entry = await client.getEntries({  //client is line 17 is creating connection 
+      content_type: type,
+      limit,
+      skip
+    })
+    setData(entry?.items)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+  
+// sure to make the connection once and then return
+
+useEffect(() => { 
+  getUser()  //we can also enter the values here of line 32 (user, 10, 0)
+
+  return () => {}
+}, [])
+
+
+// client.getSpace()
+// .then((space) => console.log(space))
+// .catch(console.error)
+
+
+  //to fetch the data from external API
+
+  // fetch('https://jsonplaceholder.typicode.com/users').then
+  // (res=>res.json())
+  // .then(data=>setData(data))
+  // .catch(err=>console.log(err))
+  
+
+  return (
+    <>
+    { 
+      data.map(e=>(
+        <div key={e?.fields.userId}> {e?.fields.userName} </div>
+      ))
+
+
+      // localData.map(e=>(
+      //  <div key={e,id}>
+      //    {e.name}
+      //  </div>
+      //))
+    }
+    </>
+  )
+}
+
+export default App
